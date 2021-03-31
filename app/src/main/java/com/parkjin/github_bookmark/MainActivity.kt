8f0cea2a9defbcc4e9bd7dff62604757f5491a22
박진 <50200481+ParkJin0318@users.noreply.github.com>
2021-03-31 15:08:29 +0900

    [init] Android 프로젝트 생성
    안드로이드 프로젝트 생성
    - Kotlin 언어
    - API 23 이상

---
+++ app/src/main/java/com/parkjin/github_bookmark/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.parkjin.github_bookmark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
