c6935970587513869797040742dfd44d030f2840
박진 <parkjin0318@naver.com>
2021-10-11 14:56:10 +0900

    add component

---
+++ presentation/src/main/java/com/parkjin/github_bookmark/component/InputFieldViewModel.kt
@@ -0,0 +1,6 @@
+package com.parkjin.github_bookmark.component
+
+import com.parkjin.github_bookmark.base.BaseViewModel
+
+class InputFieldViewModel : BaseViewModel() {
+}
--- presentation/src/main/java/com/parkjin/github_bookmark/di/ViewModelModule.kt
+++ presentation/src/main/java/com/parkjin/github_bookmark/di/ViewModelModule.kt
@@ -1,5 +1,6 @@
 package com.parkjin.github_bookmark.di
 
+import com.parkjin.github_bookmark.component.InputFieldViewModel
 import com.parkjin.github_bookmark.ui.bookmark.BookmarkViewModel
 import com.parkjin.github_bookmark.ui.github.GithubViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -9,6 +10,7 @@ import org.koin.dsl.module
  * Presentation 계층의 ViewModel 클래스 의존성 관리 모듈
  */
 val viewModelModule = module {
+    viewModel { InputFieldViewModel() }
     viewModel { GithubViewModel(get(), get()) }
     viewModel { BookmarkViewModel(get(), get()) }
 }
