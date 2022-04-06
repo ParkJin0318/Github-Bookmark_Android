f07251c21a9b943d9214f82e9cfdaf61fc27ca97
박진 <parkjin0318@naver.com>
2022-04-07 00:53:06 +0900

    add: develop component 추가

---
+++ component/src/main/java/com/parkjin/github_bookmark/component/InputFieldView.kt
@@ -0,0 +1,24 @@
+package com.parkjin.github_bookmark.component
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class InputFieldView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyle: Int = 0
+) : ConstraintLayout(context, attributeSet, defStyle) {
+
+    internal val txtInput: AppCompatEditText
+    internal val imgSearch: AppCompatImageView
+
+    init {
+        inflate(context, R.layout.view_input_field, this)
+        txtInput = findViewById(R.id.txt_input)
+        imgSearch = findViewById(R.id.img_search)
+        txtInput.setHint(R.string.input_field_hint)
+    }
+}
